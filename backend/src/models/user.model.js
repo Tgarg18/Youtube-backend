1eb80e934f4da095650b656dd9393cc2dd748941
Tgarg18 <tushargarg70091@gmail.com>
2024-06-07 19:32:47 +0530

    login_logout

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -58,7 +58,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.genegrateAccessToken = function () {
+userSchema.methods.generateAccessToken = function () {
     return jwt.sign({
         _id: this._id,
         email: this.email,
@@ -69,7 +69,7 @@ userSchema.methods.genegrateAccessToken = function () {
     })
 }
 
-userSchema.methods.genegrateRefreshToken = function () {
+userSchema.methods.generateRefreshToken = function () {
     return jwt.sign({
         _id: this._id,
     }, process.env.REFRESH_TOKEN_SECRET, {
